dce3cb24100b38aec4f92538e4a58ca24a4acd7e
zS1m <917451540@qq.com>
2023-08-01 12:54:26 +0800

    美化导航栏，新增关于页面

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,10 +2,35 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="bg-white/80 p-4 sticky top-0 drop-shadow-xl backdrop-blur z-10">
-      <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
-        <Link href="/" className="text-zinc-700/90 hover:text-zinc-700 no-underline">凝结尾迹</Link>
+      <div className="sticky top-0 bg-white/70 drop-shadow-md backdrop-blur z-10">
+        <nav className="mx-auto py-4 px-4 md:px-6">
+          <ul className="flex space-x-6 text-lg md:text-xl justify-start">
+            {[
+              ['主页', '/'],
+              ['关于', '/about']
+            ].map(([title, url]) => (
+              <li key={url}>
+                <NavLink href={url}>{title}</NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
-    </nav>
   )
+}
+
+const NavLink = ({ href, children }) => {
+  return (
+    <Link
+      className="
+        relative after:absolute after:bg-stone-500
+        after:-bottom-0.5 after:left-0 after:h-[2px]
+        after:w-full after:origin-bottom-right after:scale-x-0
+        hover:after:origin-bottom-left hover:after:scale-x-100
+        after:transition-transform after:ease-in-out after:duration-300"
+      href={href}
+    >
+      {children}
+    </Link>
+  );
 }
