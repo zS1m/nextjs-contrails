0724c63335f8ebbd5bdf855e7b92a844f2d19dc8
zS1m <917451540@qq.com>
2023-10-11 23:34:01 +0800

    文章主体

--- app/posts/[...slug]/page.tsx
+++ app/posts/[...slug]/page.tsx
@@ -1,4 +1,8 @@
-import { allPosts } from 'contentlayer/generated';
+import PageTitle from '@/components/PageTitle';
+import PostLayout from '@/layouts/PostLayout';
+import { MDXLayoutRenderer } from '@/components/MDXLayoutRenderer';
+import { allPosts, Post } from 'contentlayer/generated';
+import { sortPosts } from '@/lib/utils';
 
 type Props = {
   params: {
@@ -15,5 +19,39 @@ export async function generateStaticParams() {
 
 export default function Page({ params }: Props) {
   const slug = decodeURI(params.slug.join('/'));
-  return <div>slug: {slug}</div>;
+  const sortedPosts = sortPosts(allPosts);
+  const postIndex = sortedPosts.findIndex((post) => post.slug === slug);
+  // 未找到该文章
+  if (postIndex === -1) {
+    return (
+      <div className="mt-24 text-center">
+        <PageTitle>
+          Under Construction{' '}
+          <span role="img" aria-label="roadwork sign">
+          🚧
+        </span>
+        </PageTitle>
+      </div>
+    )
+  }
+
+  const prev = sortedPosts[postIndex + 1];
+  const next = sortedPosts[postIndex - 1];
+  const post = allPosts.find((post) => post.slug === slug) as Post;
+
+  const authorList = post?.authors || ['default'];
+  const authorDetails = [];
+  const jsonLd = post.structuredData;
+
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+      <PostLayout content={post} authorDetails={[]}>
+        <MDXLayoutRenderer code={post.body.code} />
+      </PostLayout>
+    </>
+  );
 }
---
+++ layouts/PostLayout.tsx
@@ -0,0 +1,109 @@
+import { ReactNode } from 'react'
+import type { Post } from 'contentlayer/generated'
+import Link from '@/components/Link'
+import PageTitle from '@/components/PageTitle'
+import SectionContainer from '@/components/SectionContainer'
+import Tag from '@/components/Tag'
+import siteMetadata from '@/assets/siteMetadata'
+import ScrollTopAndComment from '@/components/ScrollTopAndComment'
+
+const postDateTemplate: Intl.DateTimeFormatOptions = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+}
+
+interface LayoutProps {
+  content: Post
+  authorDetails: []
+  next?: { path: string; title: string }
+  prev?: { path: string; title: string }
+  children: ReactNode
+}
+
+export default function PostLayout({ content, authorDetails, next, prev, children }: LayoutProps) {
+  const { filePath, path, slug, date, title, tags } = content
+  const basePath = path.split('/')[0]
+
+  return (
+    <SectionContainer>
+      <ScrollTopAndComment />
+      <article>
+        <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
+          <header className="pt-6 xl:pb-6">
+            <div className="space-y-1 text-center">
+              <dl className="space-y-10">
+                <div>
+                  <dt className="sr-only">Published on</dt>
+                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                    <time dateTime={date}>
+                      {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
+                    </time>
+                  </dd>
+                </div>
+              </dl>
+              <div>
+                <PageTitle>{title}</PageTitle>
+              </div>
+            </div>
+          </header>
+          <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0">
+            <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
+              <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
+            </div>
+            <footer>
+              <div className="divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 xl:col-start-1 xl:row-start-2 xl:divide-y">
+                {tags && (
+                  <div className="py-4 xl:py-8">
+                    <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
+                      Tags
+                    </h2>
+                    <div className="flex flex-wrap">
+                      {tags.map((tag) => (
+                        <Tag key={tag} text={tag} />
+                      ))}
+                    </div>
+                  </div>
+                )}
+                {(next || prev) && (
+                  <div className="flex justify-between py-4 xl:block xl:space-y-8 xl:py-8">
+                    {prev && prev.path && (
+                      <div>
+                        <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
+                          Previous Article
+                        </h2>
+                        <div className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+                          <Link href={`/${prev.path}`}>{prev.title}</Link>
+                        </div>
+                      </div>
+                    )}
+                    {next && next.path && (
+                      <div>
+                        <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
+                          Next Article
+                        </h2>
+                        <div className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+                          <Link href={`/${next.path}`}>{next.title}</Link>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                )}
+              </div>
+              <div className="pt-4 xl:pt-8">
+                <Link
+                  href={`/`}
+                  className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                  aria-label="Back to the blog"
+                >
+                  &larr; Back to the blog
+                </Link>
+              </div>
+            </footer>
+          </div>
+        </div>
+      </article>
+    </SectionContainer>
+  )
+}
