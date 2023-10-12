50278e0e8c9f85b72d8c14bf35dc7695b1c043cd
zS1m <917451540@qq.com>
2023-10-12 21:47:20 +0800

    新增sitemap及robots生成

---
+++ app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from 'next';
+import { allPosts } from 'contentlayer/generated';
+import siteMetadata from '@/assets/siteMetadata';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const siteUrl = siteMetadata.siteUrl;
+  const blogRoutes = allPosts.map((post) => ({
+    url: `${siteUrl}/${post.path}`,
+    lastModified: post.lastmod || post.date
+  }));
+
+  const routes = ['', 'posts', 'about'].map((route) => ({
+    url: `${siteUrl}/${route}`,
+    lastModified: new Date().toISOString().split('T')[0]
+  }))
+
+  return [...routes, ...blogRoutes];
+}
