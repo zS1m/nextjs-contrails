55e7ffcf4bb3565532a398bc66d00843ffb3d1b9
zS1m <917451540@qq.com>
2023-10-14 01:42:26 +0800

    修改Header及依赖

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,43 +1,39 @@
-import Image from 'next/image';
 import Link from '@/components/Link';
+import Logo from '@/public/logo.svg';
 import headerNavLinks from '@/assets/headerNavLinks';
+import siteMetadata from '@/assets/siteMetadata';
 
 export default function Header() {
   return (
     <header className="flex items-center justify-between py-10">
-      <Link href="/" className="flex items-center hover:text-cyan-600 select-none transition-colors duration-300">
-        <Image alt="凝结尾迹" src="/avatar.svg" width="42" height="42" priority></Image>
-        <span className="ml-2 text-3xl font-semibold font-mono">凝结尾迹</span>
-      </Link>
+      <div>
+        <Link href="/" aria-label={siteMetadata.headerTitle}>
+          <div className="flex items-center justify-between">
+            <div className="mr-3">
+            </div>
+            {typeof siteMetadata.headerTitle === 'string' ? (
+              <div className="hidden h-6 text-2xl font-semibold sm:block">
+                {siteMetadata.headerTitle}
+              </div>
+            ) : (
+              siteMetadata.headerTitle
+            )}
+          </div>
+        </Link>
+      </div>
       <div className="flex items-center leading-5 space-x-4 sm:space-x-6">
         {headerNavLinks
           .filter((link) => link.href !== '/')
           .map((link) => (
-            <NavLink
+            <Link
               key={link.title}
               href={link.href}
+              className="hidden sm:block font-medium text-gray-900 dark:text-gray-100"
             >
               {link.title}
-            </NavLink>
-        ))}
+            </Link>
+          ))}
       </div>
     </header>
-  );
-}
-
-const NavLink = ({ href, children }: { href: string, children: string }) => {
-  return (
-    <Link
-      className="
-        hidden sm:block font-medium text-gray-900 dark:text-gray-100
-        relative after:absolute after:bg-stone-500
-        after:-bottom-0.5 after:left-0 after:h-[2px]
-        after:w-full after:origin-bottom-right after:scale-x-0
-        hover:after:origin-bottom-left hover:after:scale-x-100
-        after:transition-transform after:ease-in-out after:duration-300"
-      href={href}
-    >
-      {children}
-    </Link>
   );
 }
