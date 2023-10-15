913c3741d75cf3d031f15fc2a10c59f75f6dabcb
zS1m <917451540@qq.com>
2023-10-15 18:35:05 +0800

    依赖及配置项清理

--- tailwind.config.js
+++ tailwind.config.js
@@ -66,7 +66,6 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio')
+    require('@tailwindcss/typography')
   ],
 }
