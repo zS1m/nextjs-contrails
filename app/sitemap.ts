f43d37f162f777e26573f70aa7f59884445fb73d
zS1m <917451540@qq.com>
2023-11-03 13:21:19 +0800

    sitemap文章url修改

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,10 +4,13 @@ import siteMetadata from '@/assets/siteMetadata';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = siteMetadata.siteUrl;
-  const blogRoutes = allPosts.map((post) => ({
-    url: `${siteUrl}/${post.path}`,
-    lastModified: post.lastmod || post.date
-  }));
+  const blogRoutes = allPosts.map((post) => {
+    const basePath = post.path.split('/')[0];
+    return {
+      url: `${siteUrl}/${basePath}/${post.url}`,
+      lastModified: post.lastmod || post.date
+    };
+  });
 
   const routes = ['', 'posts', 'tags', 'about'].map((route) => ({
     url: `${siteUrl}/${route}`,
