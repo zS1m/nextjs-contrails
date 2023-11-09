a1b650a7125c020bca86904dbfd6a6574a549ce1
zS1m <917451540@qq.com>
2023-11-09 22:21:13 +0800

    切换主题过渡视觉效果

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,7 +69,7 @@ export default function RootLayout({
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
-      <body className="bg-white text-black antialiased dark:bg-gray-950 dark:text-white">
+      <body className="bg-white text-black antialiased dark:bg-gray-950 dark:text-white transition-colors duration-200">
         <ThemeProviders>
           <GA googleAnalyticsId={googleAnalyticsId} />
           <SectionContainer>
