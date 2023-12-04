43d71570cdd69c678ee496f7cf8e556f31b5fada
zS1m <917451540@qq.com>
2023-12-04 23:06:44 +0800

    feature: Add lightbox for images

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,5 +1,32 @@
-import NextImage, { ImageProps } from 'next/image';
+'use client';
 
-const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
+import NextImage, { ImageProps } from 'next/image';
+import { useRef, useEffect } from 'react';
+import { Fancybox } from '@fancyapps/ui';
+import '@fancyapps/ui/dist/fancybox/fancybox.css';
+
+const Image = ({ ...rest }: ImageProps) => {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+
+    const delegate = '[data-fancybox]';
+    const options = {};
+
+    Fancybox.bind(container, delegate, options);
+
+    return () => {
+      Fancybox.unbind(container);
+      Fancybox.close();
+    };
+  }, []);
+
+  return (
+    <figure ref={containerRef} className="hover:cursor-pointer">
+      <NextImage data-fancybox="single" data-caption={rest.alt} width={800} height={800} {...rest} />
+    </figure>
+  );
+}
 
 export default Image;
