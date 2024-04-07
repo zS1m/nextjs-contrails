ed6f9c1954cafbf2a9b9cefef39e1e5d879f741f
zS1m <917451540@qq.com>
2024-04-07 23:43:41 +0800

    fix: hide tags and sitemap url of draft posts in prod env

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,9 +2,13 @@ import { MetadataRoute } from 'next';
 import { allPosts } from 'contentlayer/generated';
 import siteMetadata from '@/assets/siteMetadata';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = siteMetadata.siteUrl;
-  const blogRoutes = allPosts.map((post) => {
+
+  // 文章
+  const blogRoutes = allPosts.filter((post) => !(isProduction && post.draft)).map((post) => {
     const basePath = post.path.split('/')[0];
     return {
       url: `${siteUrl}/${basePath}/${post.url}`,
@@ -12,6 +16,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     };
   });
 
+  // 导航页
   const routes = ['', 'posts'].map((route) => ({
     url: `${siteUrl}/${route}`,
     lastModified: new Date(siteMetadata.buildTime).toISOString()
--- lib/tags.tsx
+++ lib/tags.tsx
@@ -6,7 +6,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 export function getAllTags(allPosts: Post[]) {
   const tagCount: Record<string, number> = {};
   allPosts.forEach((post) => {
-    if (post.tags && (!isProduction || post.draft !== true)) {
+    if (post.tags && !(isProduction && post.draft)) {
       post.tags.forEach((tag) => {
         const formattedTag = slug(tag);
         if (formattedTag in tagCount) {
