fb808fefbafb4b62e1a89b55a287ed817fc58d2d
zS1m <917451540@qq.com>
2024-04-21 11:01:08 +0800

    fix: sort the posts by year in desc, modify styles between years block

--- layouts/ArchiveLayout.tsx
+++ layouts/ArchiveLayout.tsx
@@ -26,6 +26,10 @@ export default function AuthorLayout({ posts }: Props) {
     }
     postsSortedByYear[year].push(post);
   });
+
+  // sorted posts by year
+  const sortedKeys = Object.keys(postsSortedByYear).sort((a, b) => b.localeCompare(a));
+
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
@@ -34,36 +38,38 @@ export default function AuthorLayout({ posts }: Props) {
             归档
           </h1>
         </div>
-        {
-          Object.keys(postsSortedByYear).map((year) => (
-            // 按年分组
-            <section key={year}>
-              <div className="sticky top-0 pt-4 bg-white dark:bg-gray-950 shadow-xl shadow-white dark:shadow-gray-950 z-10">
-                <h2 className="text-4xl text-gray-300 px-3 py-4 font-bold tracking-widest">{year}</h2>
-              </div>
-              <div className="border-l-4 border-primary-300 ml-3 py-16 space-y-14">
-                {
-                  postsSortedByYear[year].map((post) => (
-                    <div key={post.slug} className="relative">
-                      <div className="absolute top-1/2 -mt-2.5 -left-3 bg-white dark:bg-black h-5 w-5 rounded-full border-4 border-primary-500"></div>
-                      <div className="pl-10">
+        <div>
+          {
+            sortedKeys.map((year) => (
+              // 按年分组
+              <section key={year}>
+                <div className="sticky top-0 pt-0 bg-white dark:bg-gray-950 shadow-xl shadow-white dark:shadow-gray-950 z-10">
+                  <h2 className="text-4xl text-gray-300 px-3 py-4 font-bold tracking-widest">{year}</h2>
+                </div>
+                <div className="border-l-4 border-primary-300 ml-3 py-16 space-y-14">
+                  {
+                    postsSortedByYear[year].map((post) => (
+                      <div key={post.slug} className="relative">
+                        <div className="absolute top-1/2 -mt-2.5 -left-3 bg-white dark:bg-black h-5 w-5 rounded-full border-4 border-primary-500"></div>
+                        <div className="pl-10">
                         <span className="leading-6 text-gray-500 dark:text-gray-400">
                           {new Date(post.date).toLocaleDateString('zh-CN', postDateTemplate)}
                         </span>
-                        <h3 className="text-3xl text-gray-600 font-semibold tracking-wide mb-2 mt-1">
-                          <Link href={`/posts/${post.url}`} className="text-gray-900 dark:text-gray-100 hover:text-primary-500 dark:hover:text-primary-400">
-                            {post.title}
-                          </Link>
-                        </h3>
-                        <p className="prose max-w-none text-gray-500 dark:text-gray-400">{truncateSummary(post.summary)}</p>
+                          <h3 className="text-3xl text-gray-600 font-semibold tracking-wide mb-2 mt-1">
+                            <Link href={`/posts/${post.url}`} className="text-gray-900 dark:text-gray-100 hover:text-primary-500 dark:hover:text-primary-400">
+                              {post.title}
+                            </Link>
+                          </h3>
+                          <p className="prose max-w-none text-gray-500 dark:text-gray-400">{truncateSummary(post.summary)}</p>
+                        </div>
                       </div>
-                    </div>
-                  ))
-                }
-              </div>
-            </section>
-          ))
-        }
+                    ))
+                  }
+                </div>
+              </section>
+            ))
+          }
+        </div>
       </div>
     </>
   )
