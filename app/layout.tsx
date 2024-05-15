42e8230246b7d3a1140410edafa3e71e5a7df65b
凝结尾迹 <41533924+zS1m@users.noreply.github.com>
2024-05-15 14:15:52 +0800

    add baidu verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,6 +66,7 @@ export default function RootLayout({
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
+      <meta name="baidu-site-verification" content={siteMetadata.analytics.baiduAnalytics.baiduAnalyticsId} />
       <body className="bg-white text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
           <SectionContainer>
