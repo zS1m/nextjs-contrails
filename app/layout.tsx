e54aae4cb3bce71c07ada6096a87f332baba4e32
zS1m <917451540@qq.com>
2024-05-15 20:39:53 +0800

    feat: add google ad

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,6 +67,7 @@ export default function RootLayout({
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <meta name="baidu-site-verification" content={siteMetadata.analytics.baiduAnalytics.baiduAnalyticsId} />
+      <meta name="google-adsense-account" content="ca-pub-1140719996053837" />
       <body className="bg-white text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
           <SectionContainer>
